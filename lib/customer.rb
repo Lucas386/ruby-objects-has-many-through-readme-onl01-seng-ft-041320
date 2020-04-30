fc3c6bda63df6d7e6d81c6b7280b914b72e16c8a
Lucas Easley <lucaseasley11@gmail.com>
2020-04-30 10:41:59 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,58 +1,49 @@
 class Customer
-<<<<<<< HEAD
-    attr_accessor :meals
-    attr_reader :name, :age
-    @@all = []
-    def initialize(name,age)
-      @name = name
-      @age = age
-      @@all << self
-    end
-      def self.all
-        @@all
-      end
-      def new_meal(waiter,total,tip=0)
-        Meal.new(waiter,self,total,tip)
-        #we don't need to take customer in as an arguement as we are passing self as a reference to the current instance of customer.
-      end
-    def meals
-      Meal.all.select do |meal|
-        meal.customer == self
-        # we are iterating through every instance of Meal and returning only the ones where the meal"s customer matches the current customer.
-      end
-    end
-    def waiters
-      meals.map do |meal|
-        meal.waiter
-      end
-    end
-  end
-=======
-  attr_accessor :meals
-  attr_reader :name, :age
+  attr_accessor :name, :age
+ 
   @@all = []
-  def initialize(name,age)
+ 
+  def initialize(name, age)
     @name = name
     @age = age
     @@all << self
   end
-    def self.all
-      @@all
-    end
-    def new_meal(waiter,total,tip=0)
-      Meal.new(waiter,self,total,tip)
-      #we don't need to take customer in as an arguement as we are passing self as a reference to the current instance of customer.
-    end
+ 
+  def self.all
+    @@all
+  end
+ 
   def meals
     Meal.all.select do |meal|
       meal.customer == self
-      # we are iterating through every instance of Meal and returning only the ones where the meal"s customer matches the current customer.
     end
   end
+ 
   def waiters
     meals.map do |meal|
       meal.waiter
     end
   end
+ 
+  def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
+  end
+ 
+  def new_meal_20_percent(waiter, total)
+    tip = total * 0.2
+    Meal.new(waiter, self, total, tip)
+  end
+ 
+  def self.oldest_customer
+    oldest_age = 0
+    oldest_customer = nil
+    self.all.each do |customer|
+      if customer.age > oldest_age
+        oldest_age = customer.age
+        oldest_customer = customer
+      end
+    end
+    oldest_customer
+  end
+ 
 end
->>>>>>> d6b88df35033214509c6f8ca3bade8afdf065ebd
